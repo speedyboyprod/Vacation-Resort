44a2d14be8512c4d0e56590dd11238ce2bef1c67
Dillon Coe <SupersonicC123@gmail.com>
2023-05-04 16:12:28 -0400

    made html, adding onto js

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+"use strict";
+
+//inputs
+const nameTextEl = document.getElementById("nameText");
+const emailTextEl = document.getElementById("emailText");
+
+const checkinRateEl = document.getElementById("checkinRate");
+
+const checkinDateEl = document.getElementById("checkinDate");
+const visitNightsEl = document.getElementById("visitNights");
+
+const queenRoomEl = document.getElementById("queenRoom");
+const kingRoomEl = document.getElementById("kingRoom");
+const twoBedEl = document.getElementById("twoBed");
+
+const adultNumEl = document.getElementById("adultNum");
+const childrenNumEl = document.getElementById("childrenNum");
+
+const noDiscountEl = document.getElementById("noDiscount");
+const aaaDiscountEl = document.getElementById("aaaDiscount");
+const vetDiscountEl = document.getElementById("vetDiscount");
+
+function getRoomRate() {}
+
+checkinRateEl.onclick = function () {
+  //const baseRoomCost =
+  //const discountOption =
+  //const roomDiscount =
+  //const tax =
+  //const totalCost =
+};
